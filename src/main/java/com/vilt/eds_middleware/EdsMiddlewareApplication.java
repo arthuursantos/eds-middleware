88fe9f5d98026c55b7bbc930ec5087d981a989b2
Arthur Santos <arthur.santos@vilt.local>
2025-10-02 14:48:23 -0300

    chore: first commit

---
+++ src/main/java/com/vilt/eds_middleware/EdsMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.vilt.eds_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdsMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdsMiddlewareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vilt/eds_middleware/EdsMiddlewareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vilt.eds_middleware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdsMiddlewareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
