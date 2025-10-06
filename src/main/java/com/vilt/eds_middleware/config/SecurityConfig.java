fed37ff8c462a161303331f32d2ec03de0e16b91
Arthur Santos <arthur.santos@vilt.local>
2025-10-06 17:49:41 -0300

    debug(jwt-validation): cors config

--- src/main/java/com/vilt/eds_middleware/config/SecurityConfig.java
+++ src/main/java/com/vilt/eds_middleware/config/SecurityConfig.java
@@ -1,13 +1,20 @@
 package com.vilt.eds_middleware.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -27,14 +34,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeHttpRequests()
-                .requestMatchers(publicEndpoints).permitAll()
-                .requestMatchers(privateEndpoints).authenticated()
-                .anyRequest().denyAll()
-                .and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> {})
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(publicEndpoints).permitAll()
+                        .requestMatchers(privateEndpoints).authenticated()
+                        .anyRequest().denyAll()
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://feat-spring-middleware--aem-edge-ue--arthuursantos.aem.page"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
--- src/main/java/com/vilt/eds_middleware/controller/AuthController.java
+++ src/main/java/com/vilt/eds_middleware/controller/AuthController.java
@@ -5,13 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     @GetMapping("/login")
-    public ResponseEntity<String> login() {
-        return ResponseEntity.ok("login");
+    public ResponseEntity<Map<String, String>> login() {
+        Map<String, String> response = new HashMap<>();
+        response.put("body", "vai corinthians");
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/register")
