402ab78694b8fe43f77979398cc1b11157022de6
Arthur Santos <arthur.santos@vilt.local>
2025-11-03 14:17:06 -0300

    chore: change the EDS allowed origin to the published main

--- src/main/java/com/vilt/eds_middleware/config/SecurityConfig.java
+++ src/main/java/com/vilt/eds_middleware/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://feat-spring-middleware--aem-edge-ue--arthuursantos.aem.page"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://main--aem-edge-ue--arthuursantos.aem.page"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
